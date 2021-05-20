ac20edebaa1934355ed31dffdc7bbf4b5a0fcb85
Miquel Castro <castro-miquel@hotmail.com>
2021-05-20 22:38:47 +0200

    Fetch data from url and pass it to front

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 $(window).scroll(function () {
     let height = $(window).scrollTop();
     if (height > 0) {
-        $("nav").addClass("black-bg-nav")
+        $("nav").addClass("black-bg-nav");
     }
     if (height === 0) {
-        $("nav").removeClass("black-bg-nav")
+        $("nav").removeClass("black-bg-nav");
     }
 });
+
+let getModernDesignData = (async () => {
+    const response = await fetch("https://www.tridenia.com/maquetacio/list.php");
+    const data = await response.json();
+    data.items.forEach((item, i) => {
+        const listElement = $(".list-modern-design").children()[i];
+        $(listElement).find(".target-content span").html(item.title);
+        $(listElement).find(".target-content p").html(item.text);
+    });
+})();
